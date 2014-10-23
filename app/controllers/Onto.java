d85374f9990824a1e37b0056bfb888cee8ba0f6d
benjaminbertin <benjamin.bertin@gmail.com>
2014-10-23 12:35:06 +0200

    Missed api in the units API URL

--- app/controllers/Onto.java
+++ app/controllers/Onto.java
@@ -522,7 +522,7 @@ public class Onto extends Controller {
     protected static void initUnitsRepo() {
         if (null == unitsRepo) {
             unitsRepo = new UnitsRepoWebService();
-            ((UnitsRepoWebService) unitsRepo).setUnitsAPIURI("http://units.myc-sense.com");
+            ((UnitsRepoWebService) unitsRepo).setUnitsAPIURI("http://units.myc-sense.com/api");
             if (null != Cache.get("conversionFactors")) {
                 ((UnitsRepoCache)unitsRepo).setConversionFactorsCache((HashMap)Cache.get("conversionFactors"));
             }
